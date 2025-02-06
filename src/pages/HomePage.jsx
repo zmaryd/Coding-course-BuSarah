38586fa422170011b88a6cf051c656875c0538c9
Zahran maliq arrasyd <zahranmaliqa@gmail.com>
2025-02-07 06:49:57 +0700

    Menambahkan Card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from "../assets/img/hero.png";
 
+import { kelasTerbaru } from "../data";
+
 const HomePage = () => {
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
           <Row className="header-box d-flex align-items-center">
-            <Col lg = "6">
+            <Col lg="6">
               <h1 className="mb-4">
                 Temukan <br />
                 <span>Bakat Kreatifmu</span> Bersama Kami!
@@ -16,10 +18,10 @@ const HomePage = () => {
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta
                 excepturi non quisquam quas mollitia amet.
               </p>
-              <button className="btn btn-danger btn-lg rounded-1 me-2">
+              <button className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2">
                 Lihat Kelas
               </button>
-              <button className="btn btn-outline-danger btn-lg rounded-1 me-2">
+              <button className="btn btn-outline-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2 ">
                 Lihat Promo
               </button>
             </Col>
@@ -29,7 +31,45 @@ const HomePage = () => {
           </Row>
         </Container>
       </header>
-      <div className="kelas w-100 min-vh-100"></div>
+      <div className="kelas w-100 min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold">Kelas Terbaru</h1>
+              <p className="text-center">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            {kelasTerbaru.map((kelas) => {
+              return (
+                <Col key={kelas.id}>
+                  <img
+                    src={kelas.image}
+                    alt="unsplash.com"
+                    className="w-100 mb-5 rounded-top"
+                  />
+                  <div className="star-mb2 px-3">
+                    <i className={kelas.star1}></i>
+                    <i className={kelas.star2}></i>
+                    <i className={kelas.star3}></i>
+                    <i className={kelas.star4}></i>
+                    <i className={kelas.star5}></i>
+                  </div>
+                  <h5 className="mb-5 px-3">{kelas.title}</h5>
+                  <div className="ket -d flex justify-content-between align-items-center px-3 pb-3">
+                    <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">
+                      {kelas.buy}
+                    </button>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
